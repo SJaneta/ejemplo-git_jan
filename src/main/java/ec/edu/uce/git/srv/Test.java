2855a718effd3cb7e9458004d1bd522b512dbbee
SJANETA <smjaneta@uce.edu.ec>
2023-06-06 02:08:17 -0500

    Se agrego otro mensaje 2

--- src/main/java/ec/edu/uce/git/srv/Test.java
+++ src/main/java/ec/edu/uce/git/srv/Test.java
@@ -5,6 +5,6 @@ public class Test {
     public void saludo() {
         System.out.println("Hola Mundo, mi primer git clone");
         System.out.println("Mi nombre es Silvia");
-
+        System.out.println("Cambio 3");
     }
 }
